1d34eb76f2aacbae59b4fa07cf0c0470c1d9bd62
7tanyandreeva <7tanyandreeva@gmail.com>
2024-09-02 14:53:02 +0300

    Add files via upload

---
+++ homework1.2..py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours = 1.5
+name_of_course = 'Python'
+time_of_one_homework = number_of_hours / number_of_homework
+print('Курс:', name_of_course, ', всего задач:', number_of_homework, ', затрачено часов:', number_of_hours, ', среднее время выполнения', time_of_one_homework, 'часа')
